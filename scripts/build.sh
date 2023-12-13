a8ac43c926cd6a1abd32259e1066f3bd824ebbf8
Brian Onang'o <surgbc@gmail.com>
2023-12-13 15:47:01 +0300

    remove public dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+mkdir -p public
 rsync -av images public/
 rsync -av render public/
 rsync index.html render public/
 ORGANIZATION=$1
+
 
 sed -i "s/{{organization}}/$ORGANIZATION/g" "./public/index.html"
 
