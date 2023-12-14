3673b04f3009c1484a8f46789f354c520fccb0a7
Brian Onang'o <surgbc@gmail.com>
2023-12-14 07:29:32 +0300

    Add support for separate source repos

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,6 +14,7 @@ REPO=$2
 # sed -i "s/{{repo}}/$REPO/g" "./public/index.html"
 
 temp_file=$(mktemp)  # Create a temporary file
+sidebar_temp_file=$(mktemp)  # Create a temporary file
 
 # Function to replace non-HTML characters with their HTML equivalents
 html_safe() {
@@ -22,6 +23,20 @@ html_safe() {
     string=$(echo "$string" | sed -e 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'\''/\&#39;/g')
     echo "$string"
 }
+
+for dir in ./render/*; do
+    if [ -d "$dir" ]; then
+        # Get the directory name
+        dir_name=$(basename "$dir")
+        # Convert non-HTML characters in the directory name to HTML characters
+        html_safe_dir_name=$(html_safe "$dir_name")
+
+        # Create an HTML file with the HTML-safe directory name
+        template_file="./public/${html_safe_dir_name}.html"
+        html_path="${html_safe_dir_name}.html"
+        echo -n "<li class=\"sidebar-item\"><div class=\"sidebar-item-container\"><a href=\"./$html_path\" class=\"sidebar-item-text sidebar-link\" >$dir_name</a ></div></li>" >> "$sidebar_temp_file"
+    fi 
+done
 
 # Loop through files in the ./render directory
 for dir in ./render/*; do
@@ -33,11 +48,15 @@ for dir in ./render/*; do
 
         # Create an HTML file with the HTML-safe directory name
         template_file="./public/${html_safe_dir_name}.html"
+        html_path="${html_safe_dir_name}.html"
         cp index.html "$template_file"
 
+        
         sed -i "s/{{organization}}/$ORGANIZATION/g" "$template_file"
         sed -i "s/{{repo}}/$REPO/g" "$template_file"
         sed -i "s/{{source}}/$html_safe_dir_name/g" "$template_file"
+        sidebarItems=$(cat "$sidebar_temp_file")
+        sed -i "s/{{sidebar}}/$sidebarItems/g" "$template_file"
         
         # Loop through files in the directory
         for file in "$dir"*; do
@@ -48,6 +67,7 @@ for dir in ./render/*; do
                 # Creating the section with the filename and appending to the temporary file
                 echo "<div class=\"quarto-layout-row quarto-layout-valign-top\"><div class=\"quarto-layout-cell quarto-layout-cell-subref\" style=\"flex-basis: 100%; justify-content: center\" ><div id=\"fig-${filename_no_extension}\" class=\"quarto-figure quarto-figure-center anchored\" ><figure class=\"figure\"><p><img src=\"/$REPO/render/${filename}/${filename}.png\" class=\"img-fluid figure-img\" data-ref-parent=\"fig-figure3.1\" /></p><p></p><figcaption class=\"figure-caption\"> ${filename_no_extension} </figcaption><p></p></figure></div></div></div>" >> "$temp_file"
                 sed -i "s/{{section}}/$(sed 's:/:\\/:g' $temp_file | tr -d '\n')/g" "$template_file"
+                
             # fi
         done
     fi
