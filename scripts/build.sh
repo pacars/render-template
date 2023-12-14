cb33a95b287f445a3effc80a92f8f9b2d808b6e8
Brian Onang'o <surgbc@gmail.com>
2023-12-14 08:24:24 +0300

    Add inner navigation links

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,6 +14,7 @@ REPO=$2
 # sed -i "s/{{repo}}/$REPO/g" "./public/index.html"
 
 temp_file=$(mktemp)  # Create a temporary file
+temp_file_for_links=$(mktemp)  # Create a temporary file
 sidebar_temp_file=$(mktemp)  # Create a temporary file
 
 # Function to replace non-HTML characters with their HTML equivalents
@@ -57,15 +58,20 @@ for dir in ./render/*; do
         
         # Loop through files in the directory
         echo "" > "$temp_file"
+        echo "" > "$temp_file_for_links"
         for file in "$dir/"*; do
             # if [ -f "$file" ]; then
                 ls -lha "$dir"
                 filename=$(basename "$file")
                 filename_no_extension="${filename%.*}"
                 # Creating the section with the filename and appending to the temporary file
-                echo "<div class=\"quarto-layout-row quarto-layout-valign-top\"><div class=\"quarto-layout-cell quarto-layout-cell-subref\" style=\"flex-basis: 100%; justify-content: center\" ><div id=\"fig-${filename_no_extension}\" class=\"quarto-figure quarto-figure-center anchored\" ><figure class=\"figure\"><p><img src=\"/$REPO/render/${html_safe_dir_name}/${filename}/${filename}.png\" class=\"img-fluid figure-img\" data-ref-parent=\"fig-figure3.1\" /></p><p></p><figcaption class=\"figure-caption\"> ${filename_no_extension} </figcaption><p></p></figure></div></div></div>" >> "$temp_file"                
+                echo "<div class=\"quarto-layout-row quarto-layout-valign-top\"><div class=\"quarto-layout-cell quarto-layout-cell-subref\" style=\"flex-basis: 100%; justify-content: center\" ><div id=\"fig-${filename_no_extension}\" class=\"quarto-figure quarto-figure-center anchored\" ><figure class=\"figure\"><p><img src=\"/$REPO/render/${html_safe_dir_name}/${filename}/${filename}.png\" class=\"img-fluid figure-img\" data-ref-parent=\"fig-figure3.1\" /></p><p></p><figcaption class=\"figure-caption\"> ${filename_no_extension} </figcaption><p></p></figure></div></div></div>" >> "$temp_file"   
+                echo "<li> <a href=\"#sec-introduction\" id=\"toc-sec-introduction\" class=\"nav-link active\" data-scroll-target=\"#fig-${filename_no_extension}\" >${filename_no_extension}</a></li>" >> "$temp_file_for_links"   
+
+                             
             # fi
         done
         sed -i "s/{{section}}/$(sed 's:/:\\/:g' $temp_file | tr -d '\n')/g" "$template_file"
+        sed -i "s/{{links}}/$(sed 's:/:\\/:g' $temp_file | tr -d '\n')/g" "$temp_file_for_links"
     fi
 done
